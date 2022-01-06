94c87957d32b83dbab35eccd120615ed73ca997e
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2022-01-06 17:43:33 +0100

    Update steam_api.py

--- steam_api.py
+++ steam_api.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import request
 from flask import make_response
-from markupsafe import escape
 import steam_backend
+import gevent.monkey
+gevent.monkey.patch_all()
 
 app = Flask("SteamTest")
 dec = ""
@@ -10,7 +11,7 @@ namelen = 6
 
 @app.route('/')
 def home():
-	return "SlejmUr Test (API) Server ALIVE!\n" + "/api/enc or /api/dec"
+	return "SlejmUr Test (API) Server ALIVE!\n" #+ "/api/enc or /api/dec"
 
 """
 @app.route('/api/token/')
@@ -29,17 +30,31 @@ def api_download(name):
   return resp
 """
 @app.route('/api/steam2fa/',methods = ['GET', 'POST'])
-async def token():
+def token():
     uname = request.headers.get('username')
     pwd = request.headers.get('password')
     is2fa = request.headers.get('has2fa')
-    if is2fa is not None:
+    if is2fa is None:
       is2fa = "no"
     code = request.headers.get('code')
     codetype = request.headers.get('codetype')
     if request.method == 'POST':
-        resp = await steam_backend.pass_stuff(uname,pwd,code,is2fa,codetype)
-        rsp = make_response(resp)
+        worker = steam_backend.SteamWorker()
+        if is2fa == "no":    
+          resp = worker.login(uname,pwd)
+        else:
+          resp = worker.login2fa(uname,pwd,code,codetype)   
+        print(resp)
+        strep = str(resp)
+        strep = strep.replace("<","")
+        strep = strep.replace(">","")
+        strep = strep.replace(":"," -")
+        strep = strep.replace("EResult.","")
+        if strep == "OK":
+          strep = worker.get_has_r6()
+        #resp = steam_backend.pass_stuff(uname,pwd,code,is2fa,codetype)
+        rsp = make_response(strep)
+        
         return rsp
     if request.method == 'GET':
         rsp = make_response("Use POST method!")
@@ -62,6 +77,6 @@ def error_500(error):
     return resp
 
 def run():
-	app.run(host='0.0.0.0', port=25565)
+  app.run(host='0.0.0.0', port=25565)
         
 run()
